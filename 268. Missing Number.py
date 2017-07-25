65ea905f58323800f417c889694a12c54fe1b3d5
cherryzoe <jamie.zz815@gmail.com>
2017-07-25 11:09:09 -0400

    Update 268. Missing Number.py

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,12 +1,12 @@
-Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+# Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
 
-For example,
-Given nums = [0, 1, 3] return 2.
+# For example,
+# Given nums = [0, 1, 3] return 2.
 
-Note:
-Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+# Note:
+# Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
 
-be careful that return type is int not set or list
+# be careful that return type is int not set or list
 
 class Solution(object):
     def missingNumber(self, nums):
