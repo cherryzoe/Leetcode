43fbef6f80e68488d791d6f02e733fb702137f2d
cherryzoe <jamie.zz815@gmail.com>
2017-07-30 22:31:16 -0400

    Update 189. Rotate Array.py

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -1,12 +1,12 @@
-Rotate an array of n elements to the right by k steps.
+# Rotate an array of n elements to the right by k steps.
 
-For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+# For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
 
-Note:
-Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+# Note:
+# Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 
 
-solution 1: (correct result run in local but incorrect with online judge)
+# solution 1: (correct result run in local but incorrect with online judge) why?
 class Solution(object):
     def rotate(self, nums, k):
         """
@@ -22,10 +22,30 @@ class Solution(object):
             lst.append(nums[i])
         nums = lst
         
-wrong answer:
-[1,2]
-1
-Output:
-[1,2]
-Expected:
-[2,1]        
+# wrong answer:
+# [1,2]
+# 1
+# Output:
+# [1,2]
+# Expected:
+# [2,1]        
+
+#solution2(wrong answer) - wrong case - input [1,2] k=3
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        lst = []
+        n = len(nums)
+        if n < 2:
+            return nums
+        else:
+            for i in range(n-k, n):
+                lst.append(nums[i])
+            for i in reversed(range(k,n)):
+                nums[i] = nums[i-k]
+            for i in range(k):
+                nums[i] = lst[i]
