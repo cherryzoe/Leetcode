61231be9d01ac51fcc46a466a7b3810236659753
cherryzoe <jamie.zz815@gmail.com>
2017-07-31 21:42:33 -0400

    Update 189. Rotate Array.py

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -6,16 +6,16 @@
 # Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 
 #solution 3:
-Original List                   : 1 2 3 4 5 6 7
-After reversing all numbers     : 7 6 5 4 3 2 1
-After reversing first k numbers : 5 6 7 4 3 2 1
-After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
-https://leetcode.com/articles/rotate-array/ 
+# Original List                   : 1 2 3 4 5 6 7
+# After reversing all numbers     : 7 6 5 4 3 2 1
+# After reversing first k numbers : 5 6 7 4 3 2 1
+# After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
+# https://leetcode.com/articles/rotate-array/ 
 
-Note: 
-    1. reverse() parameter end should always less than n-1, otherwise index will out of range
-    2. k could be greater than n, so k%n => k. (a number rotate n time would come back to the original place.)
-    3. use start,end for reverse is much better than index, 1/2 length. 
+# Note: 
+#     1. reverse() parameter end should always less than n-1, otherwise index will out of range
+#     2. k could be greater than n, so k%n => k. (a number rotate n time would come back to the original place.)
+#     3. use start,end for reverse is much better than index, 1/2 length. 
 
     class Solution(object):
     def rotate(self, nums, k):
