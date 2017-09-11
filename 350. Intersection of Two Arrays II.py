d69b8daa8293a9dbfe621c14e922a0bb3802f188
cherryzoe <jamie.zz815@gmail.com>
2017-09-11 15:18:23 -0400

    Update 350. Intersection of Two Arrays II.py

--- 350. Intersection of Two Arrays II.py
+++ 350. Intersection of Two Arrays II.py
@@ -1,17 +1,17 @@
-Given two arrays, write a function to compute their intersection.
+# Given two arrays, write a function to compute their intersection.
 
-Example:
-Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+# Example:
+# Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
 
-Note:
-Each element in the result should appear as many times as it shows in both arrays.
-The result can be in any order.
-Follow up:
-What if the given array is already sorted? How would you optimize your algorithm?
-What if nums1's size is small compared to nums2's size? Which algorithm is better?
-What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+# Note:
+# Each element in the result should appear as many times as it shows in both arrays.
+# The result can be in any order.
+# Follow up:
+# What if the given array is already sorted? How would you optimize your algorithm?
+# What if nums1's size is small compared to nums2's size? Which algorithm is better?
+# What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 
-solution1:class Solution(object):
+# solution1:class Solution(object):
     def intersect(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -25,7 +25,7 @@ solution1:class Solution(object):
                 nums2.remove(i)
         return res
         
-solution2:(improved version)
+# solution2:(improved version)
 def intersect(self, nums1, nums2):
     a, b = map(collections.Counter, (nums1, nums2))
     return list((a & b).elements())
