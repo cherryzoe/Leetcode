e44f0d925e305ed8ba9e335c3054e30b4a2b6d58
cherryzoe <jamie.zz815@gmail.com>
2017-09-12 22:40:56 -0400

    Create 204. Count Primes.py

---
+++ 204. Count Primes.py
@@ -0,0 +1,26 @@
+Description:
+
+Count the number of prime numbers less than a non-negative number, n.
+
+Credits:
+Special thanks to @mithmatt for adding this problem and creating all test cases.
+
+
+solution - sieve of eratosthenes: 
+reference: 
+http://www.tangjikai.com/algorithms/leetcode-count-primes
+http://bookshadow.com/weblog/2015/04/27/leetcode-count-primes/
+
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        prime = [True] * n
+        for i in range(2, int(n**0.5)+1):
+            if prime[i]:
+                for j in range(i**2, n, i):
+                    prime[j] = False
+        return sum(prime[2:n])
+        
