3aea771d02cc4982f7969de901a77b3a9e7aff13
cherryzoe <jamie.zz815@gmail.com>
2017-09-15 13:20:15 -0400

    Update 246. Strobogrammatic Number.py

--- 246. Strobogrammatic Number.py
+++ 246. Strobogrammatic Number.py
@@ -1,8 +1,8 @@
-A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+# A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
 
-Write a function to determine if a number is strobogrammatic. The number is represented as a string.
+# Write a function to determine if a number is strobogrammatic. The number is represented as a string.
 
-For example, the numbers "69", "88", and "818" are all strobogrammatic.
+# For example, the numbers "69", "88", and "818" are all strobogrammatic.
 
 class Solution(object):
     def isStrobogrammatic(self, num):
