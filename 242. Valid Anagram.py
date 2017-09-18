b0d62becab2fe38014c1458894b897cf9d42ab1c
cherryzoe <jamie.zz815@gmail.com>
2017-09-17 20:20:12 -0400

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,15 +1,16 @@
-Given two strings s and t, write a function to determine if t is an anagram of s.
+# Given two strings s and t, write a function to determine if t is an anagram of s.
 
-For example,
-s = "anagram", t = "nagaram", return true.
-s = "rat", t = "car", return false.
+# For example,
+# s = "anagram", t = "nagaram", return true.
+# s = "rat", t = "car", return false.
 
-Note:
-You may assume the string contains only lowercase alphabets.
+# Note:
+# You may assume the string contains only lowercase alphabets.
 
-Follow up:
-What if the inputs contain unicode characters? How would you adapt your solution to such case?
+# Follow up:
+# What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
+solution1:
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -20,3 +21,19 @@ class Solution(object):
         n1 = collections.Counter(s)
         n2 = collections.Counter(t)
         return n1==n2
+
+    
+solution2:
+#     It creates a size 26 int arrays as buckets for each letter in alphabet. 
+#     It increments the bucket value with String s and decrement with string t. 
+#     So if they are anagrams, all buckets should remain with initial value which is zero. 
+#     So just checking that and return
+        n = [0] * 26
+        for i in s:
+            n[ord(i) - ord('a')] += 1
+        for j in t:
+            n[ord(j) - ord('a')] -= 1
+        for k in n:
+            if k != 0:
+                return False
+        return True
