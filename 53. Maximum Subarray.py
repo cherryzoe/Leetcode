67edf98fa9efc56eb84bb6b83a8f6a895ecfe867
cherryzoe <jamie.zz815@gmail.com>
2017-12-08 11:15:00 -0500

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,7 +1,7 @@
-Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+# Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
-For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
-the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+# For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+# the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 
 
 class Solution(object):
@@ -12,7 +12,7 @@ class Solution(object):
         """
         maxx = nums[0]
         summ = nums[0]
-       # i is from the second element for case like the input only has one element such as [1]
+# i is from the second element for case like the input only has one element such as [1]
         for i in nums[1:]:
             if i+summ > i:
                 summ += i
