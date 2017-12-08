4d2135095e3dd62630089435ff56afd03bd4f957
cherryzoe <jamie.zz815@gmail.com>
2017-12-07 21:35:20 -0500

    Create 66. Plus One.py

---
+++ 66. Plus One.py
@@ -0,0 +1,19 @@
+Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+
+You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        n = len(digits)
+        for i in range(n-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1]+[0]*n
