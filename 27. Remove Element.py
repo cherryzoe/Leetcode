56c44c13c8b955a2c492ba951fef2e5210797508
cherryzoe <jamie.zz815@gmail.com>
2017-12-10 10:42:50 -0500

    Update 27. Remove Element.py

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -1,12 +1,16 @@
-Given an array and a value, remove all instances of that value in-place and return the new length.
+# Given an array and a value, remove all instances of that value in-place and return the new length.
 
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+# The order of elements can be changed. It doesn't matter what you leave beyond the new length.
 
-Example:
-Given nums = [3,2,2,3], val = 3,
-Your function should return length = 2, with the first two elements of nums being 2.
+# Example:
+# Given nums = [3,2,2,3], val = 3,
+# Your function should return length = 2, with the first two elements of nums being 2.
+
+解题思路：
+遍历数组每一个元素与目标值比较，若相等则跳过，不等则在原数组的idx(从0开始，每替换一次idx增一）位置替换成该值（题目要求in place)。 
+最后返回新数组的长度，即idx的值
 
 class Solution(object):
     def removeElement(self, nums, val):
