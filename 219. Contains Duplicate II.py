16e3625591f8ad85eaa047678b90bd391682c21e
cherryzoe <jamie.zz815@gmail.com>
2017-12-10 12:34:34 -0500

    Update 219. Contains Duplicate II.py

--- 219. Contains Duplicate II.py
+++ 219. Contains Duplicate II.py
@@ -1,7 +1,8 @@
-# Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+# Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array 
+# such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
 
-Update on 12/10:
-Solution 3
+# Update on 12/10:
+# Solution 3
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         """
