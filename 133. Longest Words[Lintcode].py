05a4156896f9909b0503b037a3932976e334e4ad
cherryzoe <jamie.zz815@gmail.com>
2017-12-12 11:13:22 -0500

    Create 133. Longest Words[Lintcode].py

---
+++ 133. Longest Words[Lintcode].py
@@ -0,0 +1,32 @@
+Given a dictionary, find all of the longest words in the dictionary.
+Example
+Given
+
+{
+  "dog",
+  "google",
+  "facebook",
+  "internationalization",
+  "blabla"
+}
+the longest words are(is) ["internationalization"].
+
+Given
+
+{
+  "like",
+  "love",
+  "hate",
+  "yes"
+}
+the longest words are ["like", "love", "hate"].
+
+  def longestWords(self, dictionary):
+        # write your code here
+        longest = 0
+        res = []
+        dic = {n : len(n) for n in dictionary}
+        for i,v in dic.items():
+            longest = max(longest, v)
+        res = [x for x,y in dic.items() if y == longest]
+        return res
