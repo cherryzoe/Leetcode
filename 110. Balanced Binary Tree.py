5119df814d3e18c449e25e40301dea75a6ed25a4
cherryzoe <jamie.zz815@gmail.com>
2018-01-05 16:36:57 -0500

    Update 110. Balanced Binary Tree.py

--- 110. Balanced Binary Tree.py
+++ 110. Balanced Binary Tree.py
@@ -1,7 +1,6 @@
 # Given a binary tree, determine if it is height-balanced.
-
-# For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees 
-# of every node never differ by more than 1.
+# For this problem, a height-balanced binary tree is defined as a binary tree 
+# in which the depth of the two subtrees of every node never differ by more than 1.
 
 解题思路
 - 一般BST的问题，首先往divide & conquer的思路去想
@@ -29,8 +28,8 @@ class Solution(object):
         
     def maxDepth(self, root):
         if not root:
-            return 0
-          
+            return 0       
+        
         left = self.maxDepth(root.left)
         right = self.maxDepth(root.right)
         
