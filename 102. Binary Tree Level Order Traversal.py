cd06ec1f3f471dd15c2f86dc7c020894d3bbe67d
cherryzoe <jamie.zz815@gmail.com>
2018-01-05 19:18:03 -0500

    Update 102. Binary Tree Level Order Traversal.py

--- 102. Binary Tree Level Order Traversal.py
+++ 102. Binary Tree Level Order Traversal.py
@@ -1,19 +1,24 @@
-Given a binary tree, return the level order traversal of its nodes' values. 
-(ie, from left to right, level by level).
+# Given a binary tree, return the level order traversal of its nodes' values. 
+# (ie, from left to right, level by level).
 
-For example:
-Given binary tree [3,9,20,null,null,15,7],
-    3
-   / \
-  9  20
-    /  \
-   15   7
-return its level order traversal as:
-[
-  [3],
-  [9,20],
-  [15,7]
-]
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# return its level order traversal as:
+# [
+#   [3],
+#   [9,20],
+#   [15,7]
+# ]
+
+解题思路：
+-   用一个数组存放当current level 的 Node, 首先得到其所有节点的值。然后判断每个节点是否有子节点, 若有则放到nex中
+-   需要注意的是 nex 数组在每次循环开始时初始化为空
+
 
 
 class Solution(object):
