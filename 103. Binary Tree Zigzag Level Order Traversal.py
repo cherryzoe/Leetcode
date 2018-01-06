147a612d7fdd8fd56d0edbbdc092087bebf844fc
cherryzoe <jamie.zz815@gmail.com>
2018-01-05 19:56:23 -0500

    Update 103. Binary Tree Zigzag Level Order Traversal.py

--- 103. Binary Tree Zigzag Level Order Traversal.py
+++ 103. Binary Tree Zigzag Level Order Traversal.py
@@ -1,23 +1,26 @@
-Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+# Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
 
-For example:
-Given binary tree [3,9,20,null,null,15,7],
-    3
-   / \
-  9  20
-    /  \
-   15   7
-return its zigzag level order traversal as:
-[
-  [3],
-  [20,9],
-  [15,7]
-]
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# return its zigzag level order traversal as:
+# [
+#   [3],
+#   [20,9],
+#   [15,7]
+# ]
 
 解题思路：
-在101，102的基础上， 加上一个triiger判断即可
-需要注意的是，一定是在读取value的时候再reverse，而不是存储Node的时候
-若在将Node存入NexNode数组时reverse， 那么下次再遍历已反转的数组时，子节点的位置也会相应错乱。
+在#101，#102的基础上， 一行隔一行反转。 加上一个triiger判断是否需要反转即可。
+需要注意：
+    1.  一定是在读取value的时候再reverse，而不是存储Node的时候
+    若在将Node存入NexNode数组时reverse， 那么下次再遍历已反转的数组时，子节点的位置也会相应错乱。
+    2. 此解法将trigger变量初始化为1， 之后可用trigger = - trigger
+    如果用boolean, 之后用 trigger = not trigger 即可
 
 class Solution(object):
     def zigzagLevelOrder(self, root):
@@ -30,15 +33,16 @@ class Solution(object):
         
         res = []
         curNode = [root]
-        flag = 1
+        trigger = 1
         
         while curNode:
             nexNode = []
-            if flag == 1:
+            # 判断是够需要反转此level的节点
+            if trigger == 1:
                 res.append( [ node.val for node in curNode])
             else:
                 res.append( [node.val for node in curNode[::-1]])
-            flag = -flag
+            trigger = -trigger
             for node in curNode:
                 if node.left:
                     nexNode.append(node.left)
