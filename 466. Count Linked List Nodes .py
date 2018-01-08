e3e4519b5d80fe0efc7d41d2193cb8611c728899
cherryzoe <jamie.zz815@gmail.com>
2018-01-08 16:53:57 -0500

    Create 466. Count Linked List Nodes .py

---
+++ 466. Count Linked List Nodes .py
@@ -0,0 +1,14 @@
+Count how many nodes in a linked list.
+
+Have you met this question in a real interview? Yes
+Example
+Given 1->3->5, return 3
+
+ def countNodes(self, head):
+        # write your code here
+        cnt = 0
+        while head:
+            cnt += 1
+            head = head.next
+        return cnt
+     
