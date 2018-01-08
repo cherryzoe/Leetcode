926e2c703d966a92c7ffe9e84d5865401da871b4
cherryzoe <jamie.zz815@gmail.com>
2018-01-08 11:05:26 -0500

    Update 206. Reverse Linked List.py

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -1,4 +1,4 @@
-Reverse a singly linked list.
+# Reverse a singly linked list.
 
 class Solution(object):
     def reverseList(self, head):
@@ -7,6 +7,9 @@ class Solution(object):
         :rtype: ListNode
         """
         pre, cur = None, head
+        
+        # check if cur is None, is cur is None then get out the loop. 
+        #Pre is at the tail of the original list, make pre as the head of reversed List and return new head.
         while cur:
             nex = cur.next
             cur.next = pre
