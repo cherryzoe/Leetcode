9c9247b0b4cdef153c091d26b9e0b32d6e5791d2
cherryzoe <jamie.zz815@gmail.com>
2018-01-08 17:13:02 -0500

    Update 166(Lintcode) 166. Nth to Last Node in List.py

--- 166(Lintcode) 166. Nth to Last Node in List.py
+++ 166(Lintcode) 166. Nth to Last Node in List.py
@@ -1,14 +1,14 @@
-Find the nth to last element of a singly linked list. 
+# Find the nth to last element of a singly linked list. 
 
-The minimum number of nodes in list is n.
+# The minimum number of nodes in list is n.
 
-Have you met this question in a real interview? Yes
-Example
-Given a List  3->2->1->5->null and n = 2, return node  whose value is 1.
+# Have you met this question in a real interview? Yes
+# Example
+# Given a List  3->2->1->5->null and n = 2, return node  whose value is 1.
    
-Idea:
-Used two pointer fast and slow. Make fast move N steps and then slow begins move at the same pace. 
-while fast reaches the end of list, slow should stop at the position len(list) - n, which is the desired result.
+# Idea:
+# Used two pointer fast and slow. Make fast move N steps and then slow begins move at the same pace. 
+# while fast reaches the end of list, slow should stop at the position len(list) - n, which is the desired result.
    
    def nthToLast(self, head, n):
         # write your code here
@@ -23,3 +23,22 @@ while fast reaches the end of list, slow should stop at the position len(list) -
             slow = slow.next
         return slow
                 
+# Solution 2:
+#    Idea: find out the length of the list. then move length - n + 1 steps
+     
+   def nthToLast(self, head, n):
+        # write your code here
+        cur = head
+        le = 0
+        
+        if head is None:
+            return None
+        # be noted: check cur.next instead of cur. Otherwise le would be greater (one plus) the actual size
+        while cur.next:
+            le += 1
+            cur = cur.next
+        cnt = le - n + 1
+        while cnt and head:
+            head = head.next
+            cnt -= 1
+        return head
