f18febf5f92b7bd780f1bd2b878ef38273909dca
cherryzoe <jamie.zz815@gmail.com>
2018-01-08 16:50:30 -0500

    Update 83. Remove Duplicates from Sorted List.py

--- 83. Remove Duplicates from Sorted List.py
+++ 83. Remove Duplicates from Sorted List.py
@@ -1,16 +1,16 @@
-Given a sorted linked list, delete all duplicates such that each element appear only once.
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
 
-For example,
-Given 1->1->2, return 1->2.
-Given 1->1->2->3->3, return 1->2->3.
+# For example,
+# Given 1->1->2, return 1->2.
+# Given 1->1->2->3->3, return 1->2->3.
 
-Idea:
-check if the value of cur.next is target value. If it is, then skip cur.next. cur point to cur.next.next. 
-Be noted that, once cur is re pointed to skip cur.next, it still needs to check is if the re pointed node 
-is the target node in the next round. Here we do NOT take additional step to move into next node!
-That is said, there are two cases: 
-1. find the target node and skip it. Here we do NOT take additional step to move into next node!
-2. not find and keep move on to next
+# Idea:
+# check if the value of cur.next is target value. If it is, then skip cur.next. cur point to cur.next.next. 
+# Be noted that, once cur is re pointed to skip cur.next, it still needs to check is if the re pointed node 
+# is the target node in the next round. Here we do NOT take additional step to move into next node!
+# That is said, there are two cases: 
+# 1. find the target node and skip it. Here we do NOT take additional step to move into next node!
+# 2. not find and keep move on to next
 
 class Solution(object):
     def deleteDuplicates(self, head):
