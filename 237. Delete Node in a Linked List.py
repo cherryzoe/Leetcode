7a1c9c93def647a5af121d5c480f0d5841f57ee9
cherryzoe <jamie.zz815@gmail.com>
2018-01-08 11:45:58 -0500

    Create 237. Delete Node in a Linked List.py

---
+++ 237. Delete Node in a Linked List.py
@@ -0,0 +1,17 @@
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
+
+Idea: 
+Since we couldn’t enter the preceding node, we can not delete the given node. 
+We can just copy the next node to the given node and delete the next one
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        
+        node.val = node.next.val
+        node.next = node.next.next
