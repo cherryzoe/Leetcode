a31ecc1e9fba5b29d3080e810bf014e83326a1a5
cherryzoe <jamie.zz815@gmail.com>
2018-01-08 11:39:56 -0500

    Update 234. Palindrome Linked List.py

--- 234. Palindrome Linked List.py
+++ 234. Palindrome Linked List.py
@@ -1,7 +1,7 @@
-Given a singly linked list, determine if it is a palindrome.
+# Given a singly linked list, determine if it is a palindrome.
 
-Follow up:
-Could you do it in O(n) time and O(1) space?
+# Follow up:
+# Could you do it in O(n) time and O(1) space?
 
 class Solution(object):
     def isPalindrome(self, head):
@@ -12,6 +12,8 @@ class Solution(object):
         fast = cur = head       
         pre = None
         
+        # when fast reach the end of list, cur just reached the mid of the list
+        # reverse the first half and then compare with the second half.
         while fast and fast.next:
             fast = fast.next.next
             nex = cur.next 
@@ -19,6 +21,8 @@ class Solution(object):
             pre = cur
             cur = nex
             
+        # if the length of linked list is odd, then skip the middle node and start from mid+1
+        # compare Node(mid+1) => Tail VS Head => Node(mid -1)    
         if fast:
             cur = cur.next
             
