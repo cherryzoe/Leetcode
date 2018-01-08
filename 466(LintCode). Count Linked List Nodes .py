144c8907cb02bbd8ce2a4a123f6dd2da0bd5fd16
cherryzoe <jamie.zz815@gmail.com>
2018-01-08 16:54:45 -0500

    Update and rename 466. Count Linked List Nodes .py to 466(LintCode). Count Linked List Nodes .py

--- 466(LintCode). Count Linked List Nodes .py
+++ 466(LintCode). Count Linked List Nodes .py
@@ -1,8 +1,8 @@
-Count how many nodes in a linked list.
+# Count how many nodes in a linked list.
 
-Have you met this question in a real interview? Yes
-Example
-Given 1->3->5, return 3
+# Have you met this question in a real interview? Yes
+# Example
+# Given 1->3->5, return 3
 
  def countNodes(self, head):
         # write your code here
