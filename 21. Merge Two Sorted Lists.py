801ca6374a43c8a5ee57a2d09ec840624879d9b3
cherryzoe <jamie.zz815@gmail.com>
2018-01-08 10:53:42 -0500

    Update 21. Merge Two Sorted Lists.py

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -1,13 +1,14 @@
-Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+# Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
-Example:
+# Example:
 
-Input: 1->2->4, 1->3->4
-Output: 1->1->2->3->4->4
+# Input: 1->2->4, 1->3->4
+# Output: 1->1->2->3->4->4
 
-Idea:
-  - compare each node and merge into the new linked list
-  - be noted that we need to create a dummy node because as the end of merge we lose the new head. So dummy would be the best way to store the head and return.
+# Idea:
+#   - compare each node and merge into the new linked list
+#   - be noted that we need to create a dummy node because as the end of merge we lose the new head. 
+#     So dummy would be the best way to store the head and return.
   
   class Solution(object):
     def mergeTwoLists(self, l1, l2):
@@ -17,6 +18,7 @@ Idea:
         :rtype: ListNode
         """
         dummy = cur = ListNode(0) # cur is pointed to head, that means cur.next = newHead, dummy.next = newHead
+        
         while l1 and l2:
             if l1.val < l2.val:
                 cur.next = l1
@@ -26,5 +28,6 @@ Idea:
                 l2 = l2.next
             cur = cur.next
         cur.next = l1 or l2
+        
         return dummy.next
         
