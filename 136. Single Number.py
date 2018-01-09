dc1b493f380fd00071cd8416fe1ebbae8caa5c17
cherryzoe <jamie.zz815@gmail.com>
2018-01-09 17:01:24 -0500

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -1,7 +1,7 @@
-iven an array of integers, every element appears twice except for one. Find that single one.
+# iven an array of integers, every element appears twice except for one. Find that single one.
 
-Note:
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+# Note:
+# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
 Solution 1: Bit computation (fast and no extra space)
 class Solution(object):
