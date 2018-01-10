3fc27bd4fab25db5d7f2bb52021f909b95d5d13d
cherryzoe <jamie.zz815@gmail.com>
2018-01-09 21:35:48 -0500

    Update 198. House Robber.py

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -28,3 +28,27 @@ class Solution(object):
         return dp[size - 1]
 
    
+Solution 2: O(n) space
+For every house k, there are two options: either to rob it (include this house: i) or not rob it (exclude this house: e).
+
+Include this house:
+i = num[k] + e (money of  houthisse + money robbed excluding the previous house)
+
+Exclude this house:
+e = max(i, e) (max of money robbed including the previous house or money robbed excluding the previous house)
+(note that i and e of the previous step, that’s why we use tmp here to store the previous i when calculating e, to make O(1) space)
+
+Here is the code:
+
+public class Solution {
+    public int rob(int[] num) {
+        int i = 0;
+        int e = 0;
+        for (int k = 0; k<num.length; k++) {
+            int tmp = i;
+            i = num[k] + e;
+            e = Math.max(tmp, e);
+        }
+        return Math.max(i,e);
+    }
+}
