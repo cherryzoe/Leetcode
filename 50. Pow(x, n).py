a22d2ed4d194f6988bf5bc38ddeb0ae33182bb78
cherryzoe <jamie.zz815@gmail.com>
2018-01-19 22:18:32 -0500

    Update 50. Pow(x, n).py

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -1,18 +1,18 @@
-Implement pow(x, n).
+# Implement pow(x, n).
 
-Example 1:
-Input: 2.00000, 10
-Output: 1024.00000
+# Example 1:
+# Input: 2.00000, 10
+# Output: 1024.00000
 
-Example 2:
-Input: 2.10000, 3
-Output: 9.26100
+# Example 2:
+# Input: 2.10000, 3
+# Output: 9.26100
 
-Idea:
-- if n = 0: return 1
-- if n < 0: return 1/(x, -n)
-- if n = odd: x^n = (x ^ n/2)*(x ^ n/2) *x   eg: 2^7 = (2 ^ 3) * (2 ^ 3) * 2
-- if n = even x^n = (x ^ n/2)*(x ^ n/2)      eg:5^6 = (5 ^ 3) * (5 ^ 3) 
+# Idea:
+# - if n = 0: return 1
+# - if n < 0: return 1/(x, -n)
+# - if n = odd: x^n = (x ^ n/2)*(x ^ n/2) *x   eg: 2^7 = (2 ^ 3) * (2 ^ 3) * 2
+# - if n = even x^n = (x ^ n/2)*(x ^ n/2)      eg:5^6 = (5 ^ 3) * (5 ^ 3) 
 
 lass Solution(object):
     def myPow(self, x, n):
