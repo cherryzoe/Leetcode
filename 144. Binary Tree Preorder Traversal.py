50efc6794f20e4fe9be3700cd774ddaacd350b65
cherryzoe <jamie.zz815@gmail.com>
2018-01-29 11:23:57 -0500

    Update 144. Binary Tree Preorder Traversal.py

--- 144. Binary Tree Preorder Traversal.py
+++ 144. Binary Tree Preorder Traversal.py
@@ -1,17 +1,18 @@
-Given a binary tree, return the preorder traversal of its nodes' values.
+# Given a binary tree, return the preorder traversal of its nodes' values.
 
-For example:
-Given binary tree [1,null,2,3],
-   1
-    \
-     2
-    /
-   3
-return [1,2,3].
+# For example:
+# Given binary tree [1,null,2,3],
+#    1
+#     \
+#      2
+#     /
+#    3
+# return [1,2,3].
 
-If the child node is Null, still push into the stack. While pop out, judege if it's NULL then skip to the next top one on stack.
-root - left(if left has child tree, save left node val into res and push its child node into stack
-left node is on top of right node
+# Idea:
+# 1. If the child node is Null, still push into the stack. While pop out, judege if it's NULL then skip to the next top one on stack.
+# 2. root - left(if left has child tree, save left node val into res and push its child node into stack
+# 3. left node is on top of right node in stack, so while pop out left is the one prior to right node
 
 class Solution(object):
     def preorderTraversal(self, root):
