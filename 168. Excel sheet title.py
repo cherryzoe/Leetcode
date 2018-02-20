504cede2d3a6fb334f8d8315cce5520bf351ae16
cherryzoe <jamie.zz815@gmail.com>
2018-02-20 11:34:20 -0500

    Create 168. Excel sheet title.py

---
+++ 168. Excel sheet title.py
@@ -0,0 +1,28 @@
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+
+Idea:
+calculate the difference between digit and 'A', chr() function get each digit
+
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        res = ''
+        while n > 0:
+            n -= 1
+            res = chr(n%26 + ord('A')) + res
+            n = n/26
+        return res
+
