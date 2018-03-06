aaa2bdc2c938de0dfe836814e23901cf2aeb8fc3
cherryzoe <jamie.zz815@gmail.com>
2018-03-05 22:51:16 -0500

    Update 617. Merge Two Binary Trees.py

--- 617. Merge Two Binary Trees.py
+++ 617. Merge Two Binary Trees.py
@@ -1,22 +1,22 @@
-Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+# Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
 
-You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+# You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
 
-Example 1:
-Input: 
-	Tree 1                     Tree 2                  
-          1                         2                             
-         / \                       / \                            
-        3   2                     1   3                        
-       /                           \   \                      
-      5                             4   7                  
-Output: 
-Merged tree:
-	     3
-	    / \
-	   4   5
-	  / \   \ 
-	 5   4   7
+# Example 1:
+# Input: 
+# 	Tree 1                     Tree 2                  
+#           1                         2                             
+#          / \                       / \                            
+#         3   2                     1   3                        
+#        /                           \   \                      
+#       5                             4   7                  
+# Output: 
+# Merged tree:
+# 	     3
+# 	    / \
+# 	   4   5
+# 	  / \   \ 
+# 	 5   4   7
    
    
  # Definition for a binary tree node.
@@ -25,7 +25,7 @@ Merged tree:
 #         self.val = x
 #         self.left = None
 #         self.right = None
-solution2, improved code faster: reuse t1 tree instead of re create many more new nodes
+# solution2, improved code faster: reuse t1 tree instead of re create many more new nodes
 class Solution(object):
     def mergeTrees(self, t1, t2):
         """
@@ -44,7 +44,7 @@ class Solution(object):
         return node
  
  
- solution 1:    
+#  solution 1:    
   class Solution(object):
     def mergeTrees(self, t1, t2):
         """
