bc740d2cdf1c47c80b7145af74b2e472608faf26
cherryzoe <jamie.zz815@gmail.com>
2018-05-08 12:27:09 -0400

    Update 24. swap Nodes in Pairs.py

--- 24. swap Nodes in Pairs.py
+++ 24. swap Nodes in Pairs.py
@@ -1,16 +1,16 @@
 
-Given a linked list, swap every two adjacent nodes and return its head.
+# Given a linked list, swap every two adjacent nodes and return its head.
 
-Example:
+# Example:
 
-Given 1->2->3->4, you should return the list as 2->1->4->3.
-Note:
+# Given 1->2->3->4, you should return the list as 2->1->4->3.
+# Note:
 
-Your algorithm should use only constant extra space.
-You may not modify the values in the list's nodes, only nodes itself may be changed.
+# Your algorithm should use only constant extra space.
+# You may not modify the values in the list's nodes, only nodes itself may be changed.
 
 
-
+# Solution:
 class Solution(object):
     def swapPairs(self, head):
         """
