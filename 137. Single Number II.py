4d6c97a59209a8dd79628a56f133e3a176857819
cherryzoe <jamie.zz815@gmail.com>
2018-05-28 14:05:05 -0400

    Update 137. Single Number II.py

--- 137. Single Number II.py
+++ 137. Single Number II.py
@@ -13,6 +13,26 @@ Example 2:
 Input: [0,1,0,1,0,1,99]
 Output: 99
 
+Solution3: 
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in range(32):
+            count = 0
+            for n in nums:
+                // 统计第i位的1的个数
+                if ((n >> i) & 1):
+                    count += 1
+            res |= count%3 << i
+        Overflow check and convert
+        if res >= 2**31:
+            res -= 2**32
+        return res
+    
 Solution2: correct without extra memory
 idea: 
     ones: capture numbers occur once
