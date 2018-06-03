ebb14f074fe510e5a7984e8f29aa40af237adc8e
cherryzoe <jamie.zz815@gmail.com>
2018-06-03 11:35:37 -0400

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,19 +1,19 @@
-Given a 32-bit signed integer, reverse digits of an integer.
+# Given a 32-bit signed integer, reverse digits of an integer.
 
-Example 1:
+# Example 1:
 
-Input: 123
-Output: 321
-Example 2:
+# Input: 123
+# Output: 321
+# Example 2:
 
-Input: -123
-Output: -321
-Example 3:
+# Input: -123
+# Output: -321
+# Example 3:
 
-Input: 120
-Output: 21
-Note:
-Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+# Input: 120
+# Output: 21
+# Note:
+# Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
 Solution1:
 class Solution(object):
@@ -30,7 +30,7 @@ class Solution(object):
             digit = x % 10
             res = res * 10 + digit
             x /= 10
-        handle overflow issue
+#         handle overflow issue
         return res*flag if abs(res) < 0x7FFFFFFF else 0
         
   Solution2:
