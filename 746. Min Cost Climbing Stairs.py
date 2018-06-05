238451186ff948e7a78a14712d10336b70d8f736
cherryzoe <jamie.zz815@gmail.com>
2018-06-04 23:08:44 -0400

    Update 746. Min Cost Climbing Stairs.py

--- 746. Min Cost Climbing Stairs.py
+++ 746. Min Cost Climbing Stairs.py
@@ -1,19 +1,24 @@
-On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+# On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
 
-Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+# Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
 
-Example 1:
-Input: cost = [10, 15, 20]
-Output: 15
-Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
-Example 2:
-Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
-Output: 6
-Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
-Note:
-cost will have a length in the range [2, 1000].
-Every cost[i] will be an integer in the range [0, 999].
+# Example 1:
+# Input: cost = [10, 15, 20]
+# Output: 15
+# Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+# Example 2:
+# Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+# Output: 6
+# Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+# Note:
+# cost will have a length in the range [2, 1000].
+# Every cost[i] will be an integer in the range [0, 999].
 
+# 起始条件 Base case
+# 状态方程 state tranfer formula
+# 边界条件
+# 思路： 最顶层可以从两条路径到达： 前一层 或者 前两层， 取较小
+# Let dp[i] be the minimum cost to reach the i-th stair.
 class Solution(object):
     def minCostClimbingStairs(self, cost):
         """
