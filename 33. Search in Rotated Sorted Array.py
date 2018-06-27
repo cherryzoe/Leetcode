9ca689bd8fd81ccd721ef5ade428337b5c0783c7
cherryzoe <ghost@gitbook.com>
2018-06-27 14:20:24 +0000

    Updates 33. Search in Rotated Sorted Array.py
    Auto commit by GitBook Editor

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -6,6 +6,36 @@ You are given a target value to search. If found in the array return its index, 
 
 You may assume no duplicate exists in the array.
 
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        start, end = 0, len(nums)-1
+        while start <= end:
+            mid = start + (end - start)/2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[start]:
+                if target >= nums[start] and target < nums[mid]:
+                    end = mid -1
+                else:
+                    start = mid + 1
+                print 'left half', start, end, mid
+            if nums[mid] < nums[end]:
+                if target <= nums[end] and target > nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid -1
+                print 'right half', start, end, mid
+        return -1
+
+
+#wrong solution: need to figure out why it's not working below: 
 class Solution(object):
     def search(self, nums, target):
         """
