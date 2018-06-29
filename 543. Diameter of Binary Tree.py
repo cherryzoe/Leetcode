c9f4313b621eb352cb8b9a40acf1bbf6409e220d
cherryzoe <ghost@gitbook.com>
2018-06-29 12:57:27 +0000

    Updates 543. Diameter of Binary Tree.py
    Auto commit by GitBook Editor

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -12,6 +12,9 @@ Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
 Note: The length of path between two nodes is represented by the number of edges between them.
 
 
+Solution:
+For every node, length of longest path which pass it = MaxDepth of its left subtree + MaxDepth of its right subtree
+
 def diameterOfBinaryTree(self, root):
         """
         :type root: TreeNode
