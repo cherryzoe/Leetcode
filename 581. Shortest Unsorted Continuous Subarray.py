6576ea4d26facfdf0c37a1ec93f1d67245a94333
cherryzoe <jamie.zz815@gmail.com>
2018-07-05 14:27:33 -0400

    Create 581. Shortest Unsorted Continuous Subarray.py

---
+++ 581. Shortest Unsorted Continuous Subarray.py
@@ -0,0 +1,29 @@
+Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+
+You need to find the shortest such subarray and output its length.
+
+Example 1:
+Input: [2, 6, 4, 8, 10, 9, 15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+Note:
+Then length of the input array is in range [1, 10,000].
+The input array may contain duplicates, so ascending order here means <=.
+
+class Solution:
+    def findUnsortedSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        maxval, minval = nums[0], nums[-1]
+        beg, end = -1, -2
+        for i in range(1, n):
+            maxval = max(maxval, nums[i])
+            minval = min(minval, nums[n-i-1])
+            if maxval > nums[i]:
+                end = i
+            if minval < nums[n-i-1]:
+                beg = n-i-1
+        return end-beg+1
