3f2e987b2652e56896e43cfa0ea9367f10404632
cherryzoe <jamie.zz815@gmail.com>
2018-07-05 11:17:35 -0400

    Update 438. Find All Anagrams in a String.py

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -1,34 +1,34 @@
-Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+# Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
 
-Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+# Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
 
-The order of output does not matter.
+# The order of output does not matter.
 
-Example 1:
+# Example 1:
 
-Input:
-s: "cbaebabacd" p: "abc"
+# Input:
+# s: "cbaebabacd" p: "abc"
 
-Output:
-[0, 6]
+# Output:
+# [0, 6]
 
-Explanation:
-The substring with start index = 0 is "cba", which is an anagram of "abc".
-The substring with start index = 6 is "bac", which is an anagram of "abc".
-Example 2:
+# Explanation:
+# The substring with start index = 0 is "cba", which is an anagram of "abc".
+# The substring with start index = 6 is "bac", which is an anagram of "abc".
+# Example 2:
 
-Input:
-s: "abab" p: "ab"
+# Input:
+# s: "abab" p: "ab"
 
-Output:
-[0, 1, 2]
+# Output:
+# [0, 1, 2]
 
-Explanation:
-The substring with start index = 0 is "ab", which is an anagram of "ab".
-The substring with start index = 1 is "ba", which is an anagram of "ab".
-The substring with start index = 2 is "ab", which is an anagram of "ab".
+# Explanation:
+# The substring with start index = 0 is "ab", which is an anagram of "ab".
+# The substring with start index = 1 is "ba", which is an anagram of "ab".
+# The substring with start index = 2 is "ab", which is an anagram of "ab".
 
-
+#Sliding window solution. Add one element in right edge and discard one element in left edge, then compare
 class Solution(object):
     def findAnagrams(self, s, p):
         """
@@ -45,7 +45,7 @@ class Solution(object):
         for i in range(lp, ls):
             cs[s[i]] += 1
             cs[s[i-lp]] -= 1
-            # print i,cs,s[i],s[i-lp]
+            # when compare Counter, {a:0,b:1} is different with {b:0}. so we need to delete enpty elements to avoid miscompare
             if cs[s[i-lp]] == 0:
                 del cs[s[i-lp]]
             if cs == cp:
