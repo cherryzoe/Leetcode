e07a14cf184d6f664489330a166d43273b37322f
cherryzoe <jamie.zz815@gmail.com>
2018-07-10 16:34:25 -0400

    Update 280. Wiggle Sort.py

--- 280. Wiggle Sort.py
+++ 280. Wiggle Sort.py
@@ -5,6 +5,11 @@
 # Input: nums = [3,5,2,1,6,4]
 # Output: One possible answer is [3,5,1,6,2,4]
 
+题目对摇摆排序的定义有两部分：
+如果i是奇数，nums[i] >= nums[i - 1]
+如果i是偶数，nums[i] <= nums[i - 1]
+所以我们只要遍历一遍数组，把不符合的情况交换一下就行了。具体来说，如果nums[i] > nums[i - 1]， 则交换以后肯定有nums[i] <= nums[i - 1]。
+
 class Solution(object):
     def wiggleSort(self, nums):
         """
