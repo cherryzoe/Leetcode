c52bdefce927dad3b4a7a37e347f80bf20d044fd
cherryzoe <jamie.zz815@gmail.com>
2018-07-10 22:38:57 -0400

    Update 406. Queue Reconstruction by Height.py

--- 406. Queue Reconstruction by Height.py
+++ 406. Queue Reconstruction by Height.py
@@ -1,16 +1,16 @@
-Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+# Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
 
-Note:
-The number of people is less than 1,100.
+# Note:
+# The number of people is less than 1,100.
 
 
-Example
+# Example
 
-Input:
-[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+# Input:
+# [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
 
-Output:
-[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+# Output:
+# [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
 
 class Solution(object):
     def reconstructQueue(self, people):
@@ -20,9 +20,10 @@ class Solution(object):
         """
         
         res = []
-        To use key= custom sorting, 
-        remember that you provide a function that takes one value and returns the proxy value to guide the sorting.
+#         To use key= custom sorting, 
+#         remember that you provide a function that takes one value 
+#         and returns the proxy value to guide the sorting.
         people = sorted(people, key = lambda(h,k):(-h,k))
         for p in people:
-            res.insert(p[1], p)
+            res.insert(p[1], p) #insert(position, value)
         return res
