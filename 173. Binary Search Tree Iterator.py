b84906049989cc8d0511a8ec5d6fdef47c982896
cherryzoe <jamie.zz815@gmail.com>
2018-07-11 21:38:37 -0400

    Update 173. Binary Search Tree Iterator.py

--- 173. Binary Search Tree Iterator.py
+++ 173. Binary Search Tree Iterator.py
@@ -1,20 +1,20 @@
-Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+# Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
 
-Calling next() will return the next smallest number in the BST.
+# Calling next() will return the next smallest number in the BST.
 
-Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+# Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
 
 
-Tips:
-本题相当于考察了BST的非递归中序遍历
-需要maintain一个stack，首先从root开始push入栈直到最左节点
-在遍历过程中，如果某个节点存在右儿子，则继续从右儿子开始push入栈直到其最左节点
-reference https://www.jianshu.com/p/68e1b84d40e2 
+# Tips:
+# 本题相当于考察了BST的非递归中序遍历
+# 需要maintain一个stack，首先从root开始push入栈直到最左节点
+# 在遍历过程中，如果某个节点存在右儿子，则继续从右儿子开始push入栈直到其最左节点
+# reference https://www.jianshu.com/p/68e1b84d40e2 
 
-maintain a stack. 
-1. push all the left nodes of root into stack ==> smallest node should be on top of the stack
-2. pop out from the top node in stack,  read and return the value the node. 
-if the node has right child, then treat the node as root and push all the left child into stack 
+# maintain a stack. 
+# 1. push all the left nodes of root into stack ==> smallest node should be on top of the stack
+# 2. pop out from the top node in stack,  read and return the value the node. 
+# if the node has right child, then treat the node as root and push all the left child into stack 
 
 # Definition for a  binary tree node
 # class TreeNode(object):
@@ -45,8 +45,8 @@ class BSTIterator(object):
         :rtype: int
         """
         current = self.stack.pop()
-        res = current.val
-        node = current.right
+        res = current.val # 先读取并返回值
+        node = current.right #指向右节点，如果存在，将其作为root， 则继续从右儿子开始push入栈直到其最左节点
         while node:
             self.stack.append(node)
             node = node.left
