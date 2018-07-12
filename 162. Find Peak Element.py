f440eac49b3e51850f06436cba94599f3f5cdbd1
cherryzoe <jamie.zz815@gmail.com>
2018-07-12 13:42:24 -0400

    Update and rename 162. 162. Find Peak Element.py to 162. Find Peak Element.py

--- 162. Find Peak Element.py
+++ 162. Find Peak Element.py
@@ -1,24 +1,24 @@
-A peak element is an element that is greater than its neighbors.
+# A peak element is an element that is greater than its neighbors.
 
-Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+# Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
 
-The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+# The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
 
-You may imagine that nums[-1] = nums[n] = -∞.
+# You may imagine that nums[-1] = nums[n] = -∞.
 
-Example 1:
+# Example 1:
 
-Input: nums = [1,2,3,1]
-Output: 2
-Explanation: 3 is a peak element and your function should return the index number 2.
-Example 2:
+# Input: nums = [1,2,3,1]
+# Output: 2
+# Explanation: 3 is a peak element and your function should return the index number 2.
+# Example 2:
 
-Input: nums = [1,2,1,3,5,6,4]
-Output: 1 or 5 
-Explanation: Your function can return either index number 1 where the peak element is 2, 
-             or index number 5 where the peak element is 6.
+# Input: nums = [1,2,1,3,5,6,4]
+# Output: 1 or 5 
+# Explanation: Your function can return either index number 1 where the peak element is 2, 
+#              or index number 5 where the peak element is 6.
              
-Solution 1 Sequence search:             
+# Solution 1 Sequence search:             
 class Solution(object):
     def findPeakElement(self, nums):
         """
@@ -31,7 +31,7 @@ class Solution(object):
         return len(nums)-1  
         
         
- Solution 2 Binary search(iteration)
+#  Solution 2 Binary search(iteration)
  class Solution(object):
     def findPeakElement(self, nums):
         """
@@ -48,7 +48,7 @@ class Solution(object):
                 lo = m2
         return lo
  
- Solution3 Binary Search(recursive)
+#  Solution3 Binary Search(recursive)
  class Solution(object):
     def findPeakElement(self, nums):
         """
