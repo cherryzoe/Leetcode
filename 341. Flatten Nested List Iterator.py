ea752a1013ad1accc2b8e0a5d2038701bbd4477f
cherryzoe <jamie.zz815@gmail.com>
2018-07-12 10:34:54 -0400

    Update 341. Flatten Nested List Iterator.py

--- 341. Flatten Nested List Iterator.py
+++ 341. Flatten Nested List Iterator.py
@@ -1,20 +1,20 @@
-Given a nested list of integers, implement an iterator to flatten it.
+# Given a nested list of integers, implement an iterator to flatten it.
 
-Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+# Each element is either an integer, or a list -- whose elements may also be integers or other lists.
 
-Example 1:
-Given the list [[1,1],2,[1,1]],
+# Example 1:
+# Given the list [[1,1],2,[1,1]],
 
-By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
+# By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,1,2,1,1].
 
-Example 2:
-Given the list [1,[4,[6]]],
+# Example 2:
+# Given the list [1,[4,[6]]],
 
-By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
-与Flatten List类似，但是那道题用递归的方式，而这道题要求用迭代的方式。
-迭代可以利用stack来完成。先将list中所有元素从后往前压入栈中。在hasNext()中，首先判断栈是否为空，若不为空再判断栈顶元素是整数还是list，
-若是整数则返回true，若是list则移除栈顶元素，并将其中元素按从后往前压入栈中，并再次从头执行hasNext的步骤，
-直到栈为空则返回false。next则直接取出栈顶元素并返回其值（根据hasNext，一定为整数而非list）。
+# By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
+# 与Flatten List类似，但是那道题用递归的方式，而这道题要求用迭代的方式。
+# 迭代可以利用stack来完成。先将list中所有元素从后往前压入栈中。在hasNext()中，首先判断栈是否为空，若不为空再判断栈顶元素是整数还是list，
+# 若是整数则返回true，若是list则移除栈顶元素，并将其中元素按从后往前压入栈中，并再次从头执行hasNext的步骤，
+# 直到栈为空则返回false。next则直接取出栈顶元素并返回其值（根据hasNext，一定为整数而非list）。
 # """
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
