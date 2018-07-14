77ab8772b144d01fdec8e752d46e8164fe960fc0
cherryzoe <jamie.zz815@gmail.com>
2018-07-13 23:02:15 -0400

    Update 17. Letter Combinations of a Phone Number.py

--- 17. Letter Combinations of a Phone Number.py
+++ 17. Letter Combinations of a Phone Number.py
@@ -1,16 +1,16 @@
-Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
 
-A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+# A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
 
 
-Example:
+# Example:
 
-Input: "23"
-Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
-Note:
+# Input: "23"
+# Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+# Note:
 
-Although the above answer is in lexicographical order, your answer could be in any order you want.
+# Although the above answer is in lexicographical order, your answer could be in any order you want.
 
 class Solution(object):
     def letterCombinations(self, digits):
@@ -22,7 +22,7 @@ class Solution(object):
         
         if not digits:
             return []
-        初始化为空字符串，这样下一步才能与其他字符串连起来。初始化为【】的话将始终为【】
+#         初始化为空字符串，这样下一步才能与其他字符串连起来。初始化为【】的话将始终为【】
         res = [""]
         for digit in digits:
             newres = []
