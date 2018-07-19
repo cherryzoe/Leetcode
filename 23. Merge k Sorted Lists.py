761cc506d317b193888d9655b5204373e268a7dd
cherryzoe <jamie.zz815@gmail.com>
2018-07-18 23:11:50 -0400

    Create 23. Merge k Sorted Lists.py

---
+++ 23. Merge k Sorted Lists.py
@@ -0,0 +1,64 @@
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+from Queue import PriorityQueue
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        q = PriorityQueue()
+        dummy = ListNode(0)
+        cur = dummy
+        for node in lists:
+            if node:
+                q.put((node.val, node))
+        while q.qsize():
+            cur.next = q.get()[1]
+            cur = cur.next
+            if cur.next:
+                q.put((cur.next.val, cur.next))
+        return dummy.next
+        
+        
+        
+ from operator import attrgetter
+
+class Solution:
+    # @param a list of ListNode
+    # @return a ListNode
+    def mergeKLists(self, lists):
+        sorted_list = []
+        for head in lists:
+            curr = head
+            while curr is not None:
+                sorted_list.append(curr)
+                curr = curr.next
+
+        sorted_list = sorted(sorted_list, key=attrgetter('val'))
+        for i, node in enumerate(sorted_list):
+            try:
+                node.next = sorted_list[i + 1]
+            except:
+                node.next = None
+
+        if sorted_list:
+            return sorted_list[0]
+        else:
+            return None
