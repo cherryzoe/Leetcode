e789f926e904d5452a72b765c9e91420a563ad29
cherryzoe <jamie.zz815@gmail.com>
2018-07-18 20:36:51 -0400

    Update 249. Group Shifted Strings.py

--- 249. Group Shifted Strings.py
+++ 249. Group Shifted Strings.py
@@ -1,18 +1,18 @@
-Given a string, we can "shift" each of its letter to its successive letter, for example: "abc" -> "bcd". We can keep "shifting" which forms the sequence:
+# Given a string, we can "shift" each of its letter to its successive letter, for example: "abc" -> "bcd". We can keep "shifting" which forms the sequence:
 
-"abc" -> "bcd" -> ... -> "xyz"
-Given a list of strings which contains only lowercase alphabets, group all strings that belong to the same shifting sequence.
+# "abc" -> "bcd" -> ... -> "xyz"
+# Given a list of strings which contains only lowercase alphabets, group all strings that belong to the same shifting sequence.
 
-Example:
+# Example:
 
-Input: ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"],
-Output: 
-[
-  ["abc","bcd","xyz"],
-  ["az","ba"],
-  ["acef"],
-  ["a","z"]
-]
+# Input: ["abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"],
+# Output: 
+# [
+#   ["abc","bcd","xyz"],
+#   ["az","ba"],
+#   ["acef"],
+#   ["a","z"]
+# ]
 
 # create a dictinary to store: key = offset(char[i] - char[0]), value = List(strings with same key)
 # we get defaultdict(<type 'list'>, {(0, 1, 2): [u'abc', u'bcd', u'xyz'], (0, 2, 4, 5): [u'acef'], (0,): [u'a', u'z'], (0, 25): [u'az', u'ba']})
