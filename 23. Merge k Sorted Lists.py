cdeb2299f1b2e1560bdaf5bc8f234d7d349d6bab
cherryzoe <jamie.zz815@gmail.com>
2018-07-18 23:12:13 -0400

    Update 23. Merge k Sorted Lists.py

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -1,20 +1,20 @@
-Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+# Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
 
-Example:
+# Example:
 
-Input:
-[
-  1->4->5,
-  1->3->4,
-  2->6
-]
-Output: 1->1->2->3->4->4->5->6
+# Input:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# Output: 1->1->2->3->4->4->5->6
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+# # Definition for singly-linked list.
+# # class ListNode(object):
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
 
 from Queue import PriorityQueue
 class Solution(object):
