89585326a2ad711a1445da34919c17097ce8bcaf
cherryzoe <ghost@gitbook.com>
2018-07-24 02:50:41 +0000

    Updates 26. Remove Duplicates from Sorted Array.py
    Auto commit by GitBook Editor

--- 26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array.py
@@ -23,3 +23,20 @@ class Solution(object):
                     index += 1
                     nums[index] = i
             return index+1
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        idx = 0
+        for i in range(1,len(nums)):
+            if nums[i] != nums[idx]:
+                idx += 1
+                nums[idx] = nums[i]
+        return idx+1                
+                
+            
