4cc66862cb354a52f3bce7f868fc36870b547876
cherryzoe <jamie.zz815@gmail.com>
2018-08-21 19:29:19 -0400

    Update 226. Invert Binary Tree.py

--- 226. Invert Binary Tree.py
+++ 226. Invert Binary Tree.py
@@ -42,4 +42,30 @@ class Solution(object):
                 stack.append(node.right)
         return root
 
+   
+#    2.  Recursion solution
 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return 
+        self.helper(root)
+        return root
+    
+    def helper(self, node):
+        if not node:
+            return 
+        node.left, node.right = node.right, node.left
+        self.helper(node.left)
+        self.helper(node.right)
