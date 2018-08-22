598f0011877c89ffad0e64424c2016f1657f79f8
cherryzoe <jamie.zz815@gmail.com>
2018-08-21 23:39:49 -0400

    Update 232. Implement Queue using Stacks.py

--- 232. Implement Queue using Stacks.py
+++ 232. Implement Queue using Stacks.py
@@ -1,24 +1,27 @@
-Implement the following operations of a queue using stacks.
+# Implement the following operations of a queue using stacks.
 
-push(x) -- Push element x to the back of queue.
-pop() -- Removes the element from in front of queue.
-peek() -- Get the front element.
-empty() -- Return whether the queue is empty.
-Example:
+# push(x) -- Push element x to the back of queue.
+# pop() -- Removes the element from in front of queue.
+# peek() -- Get the front element.
+# empty() -- Return whether the queue is empty.
+# Example:
 
-MyQueue queue = new MyQueue();
+# MyQueue queue = new MyQueue();
 
-queue.push(1);
-queue.push(2);  
-queue.peek();  // returns 1
-queue.pop();   // returns 1
-queue.empty(); // returns false
-Notes:
+# queue.push(1);
+# queue.push(2);  
+# queue.peek();  // returns 1
+# queue.pop();   // returns 1
+# queue.empty(); // returns false
+# Notes:
 
-You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
-Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
-You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+# You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+# Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+# You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
 
+# 思路： 
+# move(): 当stack2为空，而stack1中存有元素时，将1中元素弹出并存入2以实现倒序排列
+# 只有当要pop() peek()操作之前需要以上move()步骤
 
 class MyQueue(object):
 
