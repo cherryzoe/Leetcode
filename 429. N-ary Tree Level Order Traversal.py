e8211c94471c1fb4b3b93f7fa8e3870eae26a19e
cherryzoe <jamie.zz815@gmail.com>
2018-08-22 00:08:31 -0400

    Update 429. N-ary Tree Level Order Traversal.py

--- 429. N-ary Tree Level Order Traversal.py
+++ 429. N-ary Tree Level Order Traversal.py
@@ -1,29 +1,27 @@
-Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+# Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
 
-For example, given a 3-ary tree:
+# For example, given a 3-ary tree:
 
+
+# We should return its level order traversal: 
+
+# [
+#      [1],
+#      [3,2,4],
+#      [5,6]
+# ]
  
 
+# Note:
 
- 
-We should return its level order traversal:
-
- 
-
- 
-
-[
-     [1],
-     [3,2,4],
-     [5,6]
-]
- 
-
-Note:
-
-The depth of the tree is at most 1000.
-The total number of nodes is at most 5000.
+# The depth of the tree is at most 1000.
+# The total number of nodes is at most 5000.
 """
+
+Example for testcase:children is implemented as nested dicitonary
+{"$id":"1","children":[{"$id":"2","children":[{"$id":"5","children":[],"val":5},
+{"$id":"6","children":[],"val":6}],"val":3},{"$id":"3","children":[],"val":2},{"$id":"4","children":[],"val":4}],"val":1}
+
 # Definition for a Node.
 class Node(object):
     def __init__(self, val, children):
@@ -49,3 +47,4 @@ class Solution(object):
                         nx.append(child)    
             cur = nx
         return res
+
