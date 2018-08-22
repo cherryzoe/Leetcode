df56dd068043edf880556bbbb9600928b151aeee
cherryzoe <jamie.zz815@gmail.com>
2018-08-22 18:36:41 -0400

    Create 145. Binary Tree Postorder Traversal.py

---
+++ 145. Binary Tree Postorder Traversal.py
@@ -0,0 +1,46 @@
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [3,2,1]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+Idea: 
+pre-order: root - left - right
+post-order: left - right - root
+
+if we modify pre-order into root - right - left and reverse the result so that we get post order. 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if not root:
+            return res
+        stack = [root]
+    
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return res[::-1]
