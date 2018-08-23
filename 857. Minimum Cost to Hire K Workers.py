b4026a2ea1bd82cbdea89242b3ed3cdb6ad525ad
cherryzoe <jamie.zz815@gmail.com>
2018-08-23 00:29:13 -0400

    Update 857. Minimum Cost to Hire K Workers.py

--- 857. Minimum Cost to Hire K Workers.py
+++ 857. Minimum Cost to Hire K Workers.py
@@ -1,31 +1,34 @@
-There are N workers.  The i-th worker has a quality[i] and a minimum wage expectation wage[i].
+# There are N workers.  The i-th worker has a quality[i] and a minimum wage expectation wage[i].
 
-Now we want to hire exactly K workers to form a paid group.  When hiring a group of K workers, we must pay them according to the following rules:
+# Now we want to hire exactly K workers to form a paid group.  When hiring a group of K workers, we must pay them according to the following rules:
 
-Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
-Every worker in the paid group must be paid at least their minimum wage expectation.
-Return the least amount of money needed to form a paid group satisfying the above conditions.
+# Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
+# Every worker in the paid group must be paid at least their minimum wage expectation.
+# Return the least amount of money needed to form a paid group satisfying the above conditions.
 
  
 
-Example 1:
+# Example 1:
 
-Input: quality = [10,20,5], wage = [70,50,30], K = 2
-Output: 105.00000
-Explanation: We pay 70 to 0-th worker and 35 to 2-th worker.
-Example 2:
+# Input: quality = [10,20,5], wage = [70,50,30], K = 2
+# Output: 105.00000
+# Explanation: We pay 70 to 0-th worker and 35 to 2-th worker.
+# Example 2:
 
-Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], K = 3
-Output: 30.66667
-Explanation: We pay 4 to 0-th worker, 13.33333 to 2-th and 3-th workers seperately. 
+# Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], K = 3
+# Output: 30.66667
+# Explanation: We pay 4 to 0-th worker, 13.33333 to 2-th and 3-th workers seperately. 
  
 
-Note:
+# Note:
 
-1 <= K <= N <= 10000, where N = quality.length = wage.length
-1 <= quality[i] <= 10000
-1 <= wage[i] <= 10000
-Answers within 10^-5 of the correct answer will be considered correct.
+# 1 <= K <= N <= 10000, where N = quality.length = wage.length
+# 1 <= quality[i] <= 10000
+# 1 <= wage[i] <= 10000
+# Answers within 10^-5 of the correct answer will be considered correct.
+
+# 将人员按照 (ratio, quality) 保存并按照ratio排序。 
+# 建立一个heap保存人员的quality， 当人数超过K时，从heap中剔除quality值最高的人，让人员总数保持回到K人。重新计算当前的 qsum * ratio（当前）， 建一个变量保存历低值
 
 class Solution(object):
     def mincostToHireWorkers(self, quality, wage, K):
