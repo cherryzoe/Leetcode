88f3b4497bb0ed8afbaf41d025f32c816d563e86
cherryzoe <jamie.zz815@gmail.com>
2018-08-25 14:08:18 -0400

    Update 461. Hamming Distance.py

--- 461. Hamming Distance.py
+++ 461. Hamming Distance.py
@@ -1,25 +1,21 @@
-The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+# The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+# Given two integers x and y, calculate the Hamming distance.
 
-Given two integers x and y, calculate the Hamming distance.
+# Note:
+# 0 ≤ x, y < 231.
 
-Note:
-0 ≤ x, y < 231.
+# Example:
+# Input: x = 1, y = 4
+# Output: 2
 
-Example:
+# Explanation:
+# 1   (0 0 0 1)
+# 4   (0 1 0 0)
+#        ?   ?
 
-Input: x = 1, y = 4
+# The above arrows point to positions where the corresponding bits are different.
 
-Output: 2
-
-Explanation:
-1   (0 0 0 1)
-4   (0 1 0 0)
-       ?   ?
-
-The above arrows point to positions where the corresponding bits are different.
-
-
-
+# solution1: 
 class Solution(object):
     def hammingDistance(self, x, y):
         """
@@ -30,7 +26,7 @@ class Solution(object):
         return bin(x^y).count('1')
         
         
-Solution 2: Brian Kernighan's bit counting algorithm
+# Solution 2: Brian Kernighan's bit counting algorithm
     
 class Solution(object):
 def hammingDistance(self, x, y):
@@ -38,6 +34,6 @@ def hammingDistance(self, x, y):
         cnt = 0
         while n:
             cnt += 1
-            n &= n-1
+            n &= n-1 # put the most right 1 into 0 in n
         return cnt
 
