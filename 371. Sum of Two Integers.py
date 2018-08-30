4278a0507ad7d65443a997c2675275e7a4af6a8d
cherryzoe <jamie.zz815@gmail.com>
2018-08-29 22:06:10 -0400

    Update 371. Sum of Two Integers.py

--- 371. Sum of Two Integers.py
+++ 371. Sum of Two Integers.py
@@ -9,12 +9,31 @@
 # 因此。。做这题要保证两个数在正确的范围内（本题是int，32bit）
 
 # 如何做到呢？我们知道32bit 可以表示的无符号整数位0~0xFFFFFFFF（全0~全1）
-
 # 因此，我们使用&来保证该数是32bit.
-
 # int的0和正整数范围为0~0x7FFFFFFF，int负数的范围为-0x80000000~-1,因此，大于0x7FFFFFFF的其实是最高位为1（这是符号位）。这样算出来是把最高位不当成符号位，我们还需要对负数的情况进行修正。
-
 # 在具体实现上，我们可以先 &0x7FFFFFFF 然后取反，这样，-1变为-0x80000000(-2147483648) -2变为了-0x7FFFFFFF(-2147483647) ,因此，在^0x7FFFFFFF即可。。
+  
+# recursive solution: 
+# 此题不能用+号，故可以将需要用到加号的部分想象成函数。 a+b可以想象成 F(a,b) a+b可以转化成求 (subsum + carry) --> F(subsum, carry),实质上都是一样，可以用递归解决
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        MASK = 0xFFFFFFFF
+        max_int = 0x7FFFFFFF
+        min_int = 0x80000000
+        
+        if b == 0:
+            return a if a < max_int else ~(a ^ MASK)
+        
+        _sum = (a ^ b) & MASK
+        carry = ((a & b) << 1) & MASK
+        return self.getSum(_sum, carry)
+    
+# Iterative solution:
     def getSum(self, a, b):
         """
         :type a: int
