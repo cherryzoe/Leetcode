9f70c0f3db1955b8e6ed1a43c57ebb87bf0fcb84
cherryzoe <jamie.zz815@gmail.com>
2018-09-09 19:05:06 -0400

    Update 350. Intersection of Two Arrays II.py

--- 350. Intersection of Two Arrays II.py
+++ 350. Intersection of Two Arrays II.py
@@ -11,7 +11,34 @@
 # What if nums1's size is small compared to nums2's size? Which algorithm is better?
 # What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
 
-# solution1:class Solution(object):
+two pointers:
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        n1 = sorted(nums1)
+        n2 = sorted(nums2)
+        res = []
+        
+        i,j = 0, 0
+        while i < len(n1) and j < len(n2):
+            if n1[i] == n2[j]:
+                res.append(n1[i])
+                i += 1
+                j += 1
+            elif n1[i] > n2[j]:
+                j += 1
+            else:
+                i += 1
+        return res
+        
+        
+use dictionary to count:
+
+class Solution(object):
     def intersect(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -19,13 +46,34 @@
         :rtype: List[int]
         """
         res = []
-        for i in nums1:
-            if i in nums2:
-                res.append(i)
-                nums2.remove(i)
-        return res
+        counter = collections.defaultdict(int)
         
-# solution2:(improved version)
+        for i in nums1:
+            counter[i] += 1
+        
+        
+        for n in nums2:
+            if counter[n] > 0:
+                res.append(n)
+                counter[n] -= 1
+        return res
+    
+    
+use Counter to make it cleaner:
+
+class Solution(object):
+    def intersect(self, nums1, nums2):
+
+        counts = collections.Counter(nums1)
+        res = []
+
+        for num in nums2:
+            if counts[num] > 0:
+                res += num,
+                counts[num] -= 1
+
+        return res
+    
 def intersect(self, nums1, nums2):
     a, b = map(collections.Counter, (nums1, nums2))
     return list((a & b).elements())
