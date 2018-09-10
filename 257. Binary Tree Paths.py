4fb571c563763272d89d3bee1a0d35e6fdb665c1
cherryzoe <jamie.zz815@gmail.com>
2018-09-09 22:48:11 -0400

    Update 257. Binary Tree Paths.py

--- 257. Binary Tree Paths.py
+++ 257. Binary Tree Paths.py
@@ -1,20 +1,20 @@
-Given a binary tree, return all root-to-leaf paths.
+# Given a binary tree, return all root-to-leaf paths.
 
-Note: A leaf is a node with no children.
+# Note: A leaf is a node with no children.
 
-Example:
+# Example:
 
-Input:
+# Input:
 
-   1
- /   \
-2     3
- \
-  5
+#    1
+#  /   \
+# 2     3
+#  \
+#   5
 
-Output: ["1->2->5", "1->3"]
+# Output: ["1->2->5", "1->3"]
 
-Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+# Explanation: All root-to-leaf paths are: 1->2->5, 1->3
 
 solution 1: DFS
 # Definition for a binary tree node.
