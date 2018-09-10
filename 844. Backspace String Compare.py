ec6053484ad0eb927c53ce8fd06bd7427375efd3
cherryzoe <jamie.zz815@gmail.com>
2018-09-09 20:20:11 -0400

    Update 844. Backspace String Compare.py

--- 844. Backspace String Compare.py
+++ 844. Backspace String Compare.py
@@ -1,35 +1,35 @@
-Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+# Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
 
-Example 1:
+# Example 1:
 
-Input: S = "ab#c", T = "ad#c"
-Output: true
-Explanation: Both S and T become "ac".
-Example 2:
+# Input: S = "ab#c", T = "ad#c"
+# Output: true
+# Explanation: Both S and T become "ac".
+# Example 2:
 
-Input: S = "ab##", T = "c#d#"
-Output: true
-Explanation: Both S and T become "".
-Example 3:
+# Input: S = "ab##", T = "c#d#"
+# Output: true
+# Explanation: Both S and T become "".
+# Example 3:
 
-Input: S = "a##c", T = "#a#c"
-Output: true
-Explanation: Both S and T become "c".
-Example 4:
+# Input: S = "a##c", T = "#a#c"
+# Output: true
+# Explanation: Both S and T become "c".
+# Example 4:
 
-Input: S = "a#c", T = "b"
-Output: false
-Explanation: S becomes "c" while T becomes "b".
-Note:
+# Input: S = "a#c", T = "b"
+# Output: false
+# Explanation: S becomes "c" while T becomes "b".
+# Note:
 
-1 <= S.length <= 200
-1 <= T.length <= 200
-S and T only contain lowercase letters and '#' characters.
-Follow up:
+# 1 <= S.length <= 200
+# 1 <= T.length <= 200
+# S and T only contain lowercase letters and '#' characters.
+# Follow up:
 
-Can you solve it in O(N) time and O(1) space?
+# Can you solve it in O(N) time and O(1) space?
 
-solution1: with extra O(n) space
+# solution1: with extra O(n) space
 class Solution(object):
     def backspaceCompare(self, S, T):
         """
@@ -48,9 +48,9 @@ class Solution(object):
                 stack.append(s)
         return ''.join(stack)
         
-solution2: O(1)space?
-    Iterate through the string in reverse. If we see a backspace character, the next non-backspace character is skipped. 
-    If a character isn't skipped, it is part of the final answer.
+# solution2: O(1)space?
+#     Iterate through the string in reverse. If we see a backspace character, the next non-backspace character is skipped. 
+#     If a character isn't skipped, it is part of the final answer.
     class Solution(object):
     def backspaceCompare(self, S, T):
         """
