d43af7f3cebca6e1ecde24d7c3d7dea3a03b03f9
cherryzoe <jamie.zz815@gmail.com>
2018-09-10 22:49:11 -0400

    Create 101. Symmetric Tree.py

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,73 @@
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+
+solution1: iteratively
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        q = collections.deque()
+        if not root:
+            return True
+        if not root.left or not root.right:
+            return root.left == root.right
+        q.append(root.left)
+        q.append(root.right)
+        
+        while q:
+            t1 = q.popleft()
+            t2 = q.popleft()
+            
+            if not t1 and not t2:
+                continue
+            if not t1 or not t2 or t1.val != t2.val:
+                return False
+            q.append(t1.left)
+            q.append(t2.right)
+            q.append(t1.right)
+            q.append(t2.left)
+            
+        return True
+            
+solution2: recursively
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.helper(root.left, root.right)
+        
+    def helper(self, left, right):
+        if not left or not right:
+            return left == right
+        if left.val != right.val:
+            return False
+        return self.helper(left.left, right.right) and self.helper(left.right, right.left)
+    
