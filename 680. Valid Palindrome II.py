ec3101ee09b73af3e2dae43ad46562168e959986
cherryzoe <jamie.zz815@gmail.com>
2018-09-15 17:09:24 -0400

    Update 680. Valid Palindrome II.py

--- 680. Valid Palindrome II.py
+++ 680. Valid Palindrome II.py
@@ -1,17 +1,17 @@
-Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+# Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
 
-Example 1:
-Input: "aba"
-Output: True
-Example 2:
-Input: "abca"
-Output: True
-Explanation: You could delete the character 'c'.
-Note:
-The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+# Example 1:
+# Input: "aba"
+# Output: True
+# Example 2:
+# Input: "abca"
+# Output: True
+# Explanation: You could delete the character 'c'.
+# Note:
+# The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
 
-那么当遇到不匹配的时候，我们到底是删除左边的字符，还是右边的字符呢，我们的做法是两种情况都要算一遍，只要有一种能返回true，那么结果就返回true。
-我们可以写一个子函数来判断字符串中的某一个范围内的子字符串是否为回文串，
+# 那么当遇到不匹配的时候，我们到底是删除左边的字符，还是右边的字符呢，我们的做法是两种情况都要算一遍，只要有一种能返回true，那么结果就返回true。
+# 我们可以写一个子函数来判断字符串中的某一个范围内的子字符串是否为回文串，
 
 class Solution(object):
     def validPalindrome(self, s):
