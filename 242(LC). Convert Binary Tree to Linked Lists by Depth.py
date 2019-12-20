4959aeb00fca0288586b90fd870952827c657e0e
cherryzoe <jamie.zz815@gmail.com>
2019-12-19 20:16:07 -0500

    Update 242(LC). Convert Binary Tree to Linked Lists by Depth.py

--- 242(LC). Convert Binary Tree to Linked Lists by Depth.py
+++ 242(LC). Convert Binary Tree to Linked Lists by Depth.py
@@ -1,28 +1,28 @@
-242. Convert Binary Tree to Linked Lists by Depth
-中文English
-Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+# 242. Convert Binary Tree to Linked Lists by Depth
+# 中文English
+# Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
 
-样例
-Example 1:
+# 样例
+# Example 1:
 
-Input: {1,2,3,4}
-Output: [1->null,2->3->null,4->null]
-Explanation: 
-        1
-       / \
-      2   3
-     /
-    4
-Example 2:
+# Input: {1,2,3,4}
+# Output: [1->null,2->3->null,4->null]
+# Explanation: 
+#         1
+#        / \
+#       2   3
+#      /
+#     4
+# Example 2:
 
-Input: {1,#,2,3}
-Output: [1->null,2->null,3->null]
-Explanation: 
-    1
-     \
-      2
-     /
-    3
+# Input: {1,#,2,3}
+# Output: [1->null,2->null,3->null]
+# Explanation: 
+#     1
+#      \
+#       2
+#      /
+#     3
 
 class Solution:
     # @param {TreeNode} root the root of binary tree
