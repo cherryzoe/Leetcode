76690975fed5cc31b45cc40730b58c1e0c389df6
cherryzoe <jamie.zz815@gmail.com>
2020-01-17 10:43:44 -0500

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -7,6 +7,24 @@
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
  
+
+ 1/17/2020
+ class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        dic = {value: idx for idx, value in enumerate(nums)}
+        
+        for i in range(len(nums)):
+            temp = target - nums[i]
+            if temp in dic and dic[temp] != i:
+                return [i, dic[temp]]
+        return False
+ 
  
  def twoSum(self, numbers, target):
         # write your code here
