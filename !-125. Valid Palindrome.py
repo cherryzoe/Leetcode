8a33eefb31778d9ba3aad76c2e8610da8f7daa0c
cherryzoe <jamie.zz815@gmail.com>
2020-01-17 08:08:12 -0500

    Update !-125. Valid Palindrome.py

--- !-125. Valid Palindrome.py
+++ !-125. Valid Palindrome.py
@@ -46,6 +46,27 @@ class Solution(object):
         while l < r:
             if not s[l].isalnum():
                 l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
+    
+    class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        l,r = 0, len(s)-1
+        
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
                 continue
             if not s[r].isalnum():
                 r -= 1
