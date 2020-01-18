cf02ebf1365bcf8aa77201839825a96dcc0832ec
cherryzoe <jamie.zz815@gmail.com>
2020-01-18 10:37:28 -0500

    Update 170. Two Sum III - Data structure design.py

--- 170. Two Sum III - Data structure design.py
+++ 170. Two Sum III - Data structure design.py
@@ -1,18 +1,19 @@
-Design and implement a TwoSum class. It should support the following operations: add and find.
+# Design and implement a TwoSum class. It should support the following operations: add and find.
 
-add - Add the number to an internal data structure.
-find - Find if there exists any pair of numbers which sum is equal to the value.
+# add - Add the number to an internal data structure.
+# find - Find if there exists any pair of numbers which sum is equal to the value.
 
-Example 1:
+# Example 1:
 
-add(1); add(3); add(5);
-find(4) -> true
-find(7) -> false
-Example 2:
+# add(1); add(3); add(5);
+# find(4) -> true
+# find(7) -> false
+# Example 2:
 
-add(3); add(1); add(2);
-find(3) -> true
-find(6) -> false
+# add(3); add(1); add(2);
+# find(3) -> true
+# find(6) -> false
+
 class TwoSum(object):
 
     def __init__(self):
@@ -42,7 +43,9 @@ class TwoSum(object):
         dic = self.dic
         for n in dic:
             diff = value - n
-            if diff in dic:
+            if diff in dic:  
+#                 此处代码可优化成 ：
+#                 if diff in dic and (diff != n or dic[diff] > 1): return True
                 if diff != n:
                      return True
                 elif dic[diff] > 1:
