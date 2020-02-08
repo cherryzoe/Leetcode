4186150194416a72d5447ffd997dd5187aef8e5e
cherryzoe <jamie.zz815@gmail.com>
2020-02-08 16:24:47 -0500

    Update 79. Word Search.py

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -1,19 +1,19 @@
-Given a 2D board and a word, find if the word exists in the grid.
+# Given a 2D board and a word, find if the word exists in the grid.
 
-The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+# The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
 
-Example:
+# Example:
 
-board =
-[
-  ['A','B','C','E'],
-  ['S','F','C','S'],
-  ['A','D','E','E']
-]
+# board =
+# [
+#   ['A','B','C','E'],
+#   ['S','F','C','S'],
+#   ['A','D','E','E']
+# ]
 
-Given word = "ABCCED", return true.
-Given word = "SEE", return true.
-Given word = "ABCB", return false.
+# Given word = "ABCCED", return true.
+# Given word = "SEE", return true.
+# Given word = "ABCB", return false.
 
 class Solution(object):
     def exist(self, board, word):
@@ -23,17 +23,18 @@ class Solution(object):
         :rtype: bool
         """
         self.visited = [[False for i in range(len(board[0]))] for j in range(len(board))]
+#         数组中每一个点都可能是有效字符串的起点， 只要找到任意就表示有解
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if self.dfs(board, word, 0, i, j):
                     return True
         return False
-        
-        
+       
         
     def dfs(self, board, word, idx, row, col):
         if idx == len(word):
             return True
+#           出界条件要先判断，否则在下一步调用数组【i][j]时有index out range的错误
         if row < 0 or row >= len(board) or col < 0 or col >= len(board[0]):
             return False
         
@@ -53,7 +54,7 @@ class Solution(object):
         
         if self.dfs(board, word, idx + 1, row, col - 1):
             return True
-        
+#         回溯，将改点重新改回unvisited， 意思是当前的点并未成功作为有效字符串解，因此下次还有机会被使用
         self.visited[row][col] =  False
         
         return False
