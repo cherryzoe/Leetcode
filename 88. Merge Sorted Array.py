0084cc9777c9c5f430c48aacf6a2829e452e6167
cherryzoe <jamie.zz815@gmail.com>
2020-09-03 22:31:09 -0400

    Update 88. Merge Sorted Array.py

--- 88. Merge Sorted Array.py
+++ 88. Merge Sorted Array.py
@@ -4,6 +4,33 @@
 # You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
 
 
+2020 Update:
+解题思路： 归并排序
+值得注意的是本题中最后不是新建数组而是存放到nums1中，并且知道nums1空间足够大，如果从左边开始扫的话，会存在-nums1的数还未被检查就被nums2的数覆盖了-的情况，因此，从右边开始扫。
+    class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        
+        i,j,k = m-1, n-1, m+n-1
+        
+        while i >= 0 and j >= 0:
+            if nums1[i] >= nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        
+        if j >= 0:
+            nums1[:k+1] = nums2[:j+1]
+
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
         """
