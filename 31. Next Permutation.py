7d17cab3368ccaaada0fd42a9fc5ab5a4cef77c5
cherryzoe <jamie.zz815@gmail.com>
2021-01-15 12:32:34 -0500

    Update 31. Next Permutation.py

--- 31. Next Permutation.py
+++ 31. Next Permutation.py
@@ -20,6 +20,28 @@ class Solution(object):
     def nextPermutation(self, nums):
         """
         :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        le = len(nums)
+        i = le - 1
+
+        while i > 0 and nums[i] <= nums[i-1]:
+            i -= 1
+        if i <= 0:
+            nums.reverse()
+            return
+        else:
+            j = i
+            while j < le and nums[j] > nums[i-1]:
+                j += 1
+            nums[i-1], nums[j-1] = nums[j-1], nums[i-1]
+            nums[i:] = nums[i:][::-1]
+
+
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         size = len(nums)
