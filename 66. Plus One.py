0f7bb47a41bb6553a31fdd21e174e94d72bf3005
cherryzoe <jamie.zz815@gmail.com>
2021-01-19 16:25:07 -0500

    Update 66. Plus One.py

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -22,3 +22,20 @@ class Solution(object):
             digits[i] = 0
         //consider the case like [9]
         return [1]+[0]*n
+
+# 加法通用模板，常规解法： 
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            temp = digits[i] +  carry
+            digits[i] = temp % 10
+            carry = temp / 10
+
+        if carry:
+            digits = [1] + digits
+        return digits
