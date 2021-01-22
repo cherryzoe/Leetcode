d32bcd24f707f282867c7c60fa66d77e5a01dea2
cherryzoe <jamie.zz815@gmail.com>
2021-01-22 13:13:56 -0500

    Create 105. construct binary tree from preorder and inorder traversal.py

---
+++ 105. construct binary tree from preorder and inorder traversal.py
@@ -0,0 +1,47 @@
+根据一棵树的前序遍历与中序遍历构造二叉树。
+
+注意:
+你可以假设树中没有重复的元素。
+
+例如，给出
+
+前序遍历 preorder = [3,9,20,15,7]
+中序遍历 inorder = [9,3,15,20,7]
+返回如下的二叉树：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+
+解题思路：
+前序遍历的第一个必然是根节点，找出根节点后，带入到中序遍历的数组中定位到根节点，
+此时根节点左边是左子树，右边是右子树，由于左右子树的长度在前序遍历中也是一样的，所以根据mid也可以在前序遍历中划分出左右子树。
+以此类推，带入到下一层进行递归
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+
+        if len(preorder) == 0:
+            return None
+        
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
