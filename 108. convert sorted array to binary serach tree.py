eb4e859b6191660ca1aadaaabf0588f7d9cdf314
cherryzoe <jamie.zz815@gmail.com>
2021-04-02 21:24:34 -0400

    Create 108. convert sorted array to binary serach tree.py

---
+++ 108. convert sorted array to binary serach tree.py
@@ -0,0 +1,38 @@
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+
+Example 1:
+
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+解题思路：
+BST的中序输出一定是单调递增的数组
+题目给定的输入是一个单调递增的数组，去中点为根节点，以此分割数组左半边是左子树，右半边为右子树
+继续递归左右两边，分别取各自中点继续递归子树，直到空集或者左右边界交叉
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return []
+        return self.helper(0, len(nums)-1, nums)
+
+    def helper(self, left, right, nums):
+        if left > right:
+            return None
+        mid = left + (right - left)//2
+        root = TreeNode(nums[mid])
+        root.left = self.helper(left, mid-1, nums)
+        root.right = self.helper(mid+1, right, nums)
+        return root
