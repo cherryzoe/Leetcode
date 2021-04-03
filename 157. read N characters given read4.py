36eb1c25b9a413505dfff053c9c3ca2df7d62e13
cherryzoe <jamie.zz815@gmail.com>
2021-04-03 11:40:41 -0400

    Create 157. read N characters given read4.py

---
+++ 157. read N characters given read4.py
@@ -0,0 +1,32 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution(object):
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        res = 0
+        while res < n:
+            buf4 = [''] * 4 
+            step = read4(buf4)
+            if step == 0:
+                break
+            # 这题只能直接赋值，其他append的方法在这个API不work，会报错-out of index range
+            buf[res: res+step] = buf4
+            res += step
+        return min(res, n)
