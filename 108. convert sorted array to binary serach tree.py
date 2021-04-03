ec3c13d5e0a5fa66832c072f70e76a33705b278c
cherryzoe <jamie.zz815@gmail.com>
2021-04-02 21:25:59 -0400

    Update 108. convert sorted array to binary serach tree.py

--- 108. convert sorted array to binary serach tree.py
+++ 108. convert sorted array to binary serach tree.py
@@ -7,16 +7,12 @@ Input: nums = [-10,-3,0,5,9]
 Output: [0,-3,9,-10,null,5]
 Explanation: [0,-10,5,null,-3,null,9] is also accepted:
 
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
 解题思路：
 BST的中序输出一定是单调递增的数组
 题目给定的输入是一个单调递增的数组，去中点为根节点，以此分割数组左半边是左子树，右半边为右子树
-继续递归左右两边，分别取各自中点继续递归子树，直到空集或者左右边界交叉
+继续递归左右两边，分别取各自中点继续递归子树，
+递归终止条件：直到空集或者左右边界交叉，说明所有子集都已经被递归到了，返回None
 
 class Solution(object):
     def sortedArrayToBST(self, nums):
